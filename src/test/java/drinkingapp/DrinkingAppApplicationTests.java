3648006eecd72717b3df8715f86b8a8b64c0f4a6
BeneZ <bezraat@gmail.com>
2021-08-01 18:03:34 +0200

    Initial commit

---
+++ src/test/java/drinkingapp/DrinkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package drinkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
